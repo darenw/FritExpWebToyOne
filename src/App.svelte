269b12b7e6a824d638a3d067697ff063e1750632
darenw <darenw@darenscotwilson.com>
2022-12-05 01:51:36 -0700

    Simple plain number inputs for x,u and responsive display of exp_u(x). Seems to work.

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,22 @@
   import Counter       from './lib/Counter.svelte'
   import FritexpToyOne from './lib/FritexpToyOne.svelte'
   import fritexp       from './lib/fritexp_szekeres.js'
+  
+  let xvalue; 
+  let uvalue;
+  let answer;
+  $: answer = fritexp(uvalue, xvalue);
 </script>
 
 <main>
  <h1>Fractionally Iterated Exponential</h1>
  <FritexpToyOne />
  <p>exp_1/2(0) = { fritexp(0.5, 0) }</p>
+ <label for="xvalue">x</label>
+ <input type="number" id="xvalue" name="x" bind:value={xvalue}>
+ <label for="uvalue">u</label>
+ <input type="number" id="uvalue" name="u" bind:value={uvalue}>
+ <p>exp_({uvalue})({xvalue}) = {answer}</p>
 </main>
 
 <style>
